97ba1db49a31dafa8b0a0ee7e10331ed46e63543
TigerND <tigernwh@gmail.com>
2014-05-28 16:26:02 +0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='dtx-examples',
+    version='0.9.1',
+    description='Gittip APIs Example',
+    author='Alexander Zykov',
+    author_email='tigernwh@gmail.com',
+    url='https://github.com/TigerND/dtx-examples',
+    install_requires = [
+        'dtx-core>=0.9.1',
+        'gittip-twisted>=0.1.1',
+    ],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    zip_safe=False,
+)
